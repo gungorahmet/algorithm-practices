06eda1c93622f5f4ca6492db856c401160cd9ecf
Ahmet Gungor <ahmetgungor92@gmail.com>
2019-12-20 23:30:34 +0300

    Author: Ahmet Gungor Description: Corrected as set

--- question_2/find_second_greatest_value.py
+++ question_2/find_second_greatest_value.py
@@ -4,7 +4,7 @@ Applied PEP8 (pycodestyle)
 
 Author:       Ahmet Gungor
 Date  :       20.12.2019
-Description : Question: Find second greatest value of a given list
+Description : Question: Find second greatest value of a given set
 '''
 
 
@@ -12,15 +12,15 @@ class Greatnesss():
     def __init__(self, set_input):
         self.set_input = set_input
         self.length_set = len(self.set_input)
-        print(f"\n\nList => {self.set_input}")
-        print(f"List Length => {self.length_set}\n")
+        print(f"\n\nSet => {self.set_input}")
+        print(f"Set Length => {self.length_set}\n")
 
     def find_second_greatest_element(self):
         pass
-        # TODO
+        # TODO - Find and print here second greatest value
 
 
 if __name__ == "__main__":
-    set_input = [5, 6, 13, 7, 10, 45, 3, 1, 71, 21, 5, 9, 1, 15]
+    set_input = {5, 6, 13, 7, 10, 45, 3, 1, 71, 21, 5, 9, 1, 15}
     instance = Greatnesss(set_input)
     instance.find_second_greatest_element()
