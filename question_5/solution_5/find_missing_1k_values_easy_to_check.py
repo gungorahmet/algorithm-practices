1240fae6b6042346989be7a3732602609bc7ce26
Ahmet Gungor <ahmetgungor92@gmail.com>
2020-01-13 23:37:25 +0300

    Author: Ahmet Gungor Description: Description is updated.

--- question_5/solution_5/find_missing_1k_values_easy_to_check.py
+++ question_5/solution_5/find_missing_1k_values_easy_to_check.py
@@ -4,10 +4,8 @@ Applied PEP8 (pycodestyle)
 
 Author:       Ahmet Gungor
 Date  :       13.01.2020
-Description : This problem was asked by Two Sigma;
-              You are given an unsorted list of 999,000 unique integers each from 1 and 1,000,000.
-              Find the missing 1000 numbers.
-              What is the computational and space complexity of your solution?
+Description : Easier version of 1K missing values question.
+              This script is searching 5 missing values from 20 values in 15 list size.
 '''
 
 import sys
@@ -15,11 +13,6 @@ import sys
 
 class MissingNumbers():
     def __init__(self):
-        '''
-        Assignment some values with loop for testing;
-        It is assignd from 400 to 999399 values in total 99000 unique values.
-        So from 1 to 399 and 999400 to 1,000,000 values are missing to test.
-        '''
         print(f"\n\n")
         self.main_list = [1, 5, 7, 2, 10, 6, 16, 19, 11, 8, 13, 18, 3, 17, 14]
 
