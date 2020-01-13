9deb86c19c60ff428f3c9ff9b696e5c215ebc9b1
Ahmet Gungor <ahmetgungor92@gmail.com>
2020-01-13 23:02:17 +0300

    Author: Ahmet Gungor Description: Solution is added.

--- question_5/README.md
+++ question_5/README.md
@@ -1,5 +1,5 @@
 # Find 1000 unique in 990,000 size list from first 1M numbers.
-# Status = In Progress..
+# Status = Completed.
 This problem was asked by Two Sigma<br><br>
 
 You are given an unsorted list of 999,000 unique integers, each from 1 and 1,000,000.<br>
---
+++ question_5/solution_5/find_missing_1k_values_solution.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+'''
+Applied PEP8 (pycodestyle)
+
+Author:       Ahmet Gungor
+Date  :       13.01.2020
+Description : This problem was asked by Two Sigma;
+              You are given an unsorted list of 999,000 unique integers each from 1 and 1,000,000.
+              Find the missing 1000 numbers.
+              What is the computational and space complexity of your solution?
+'''
+
+import sys
+
+
+class MissingNumbers():
+    def __init__(self):
+        '''
+        Assigment values for testing;
+        It is assignd from 400 to 999399 values in total 99000 unique values.
+        So from 1 to 399 and 999400 to 1,000,000 values are missing to test.
+        '''
+        print(f"\n\n")
+        self.main_list = [None] * 999000
+        for i in range(400, 999400):
+            self.main_list[i - 400] = i
+
+    def find_missing_values(self):
+        self.my_dict = dict()
+        self.solution_list = []
+        for value in self.main_list:
+            if value not in self.my_dict:
+                self.my_dict[value] = True
+
+        for element in range(1, 1000001):
+            if element not in self.my_dict:
+                self.solution_list.append(element)
+
+        print(self.solution_list)
+
+
+if __name__ == "__main__":
+    instance = MissingNumbers()
+    instance = instance.find_missing_values()
