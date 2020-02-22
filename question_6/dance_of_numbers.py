bba906b5eb2b317a0122830f75802f77514ed82d
Ahmet Gungor <ahmetgungor92@gmail.com>
2020-02-23 00:10:01 +0300

    dance_of_numbers question is added.

---
+++ question_6/dance_of_numbers.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+'''
+Applied PEP8 (pycodestyle)
+
+Author:       Ahmet Gungor
+Date  :       23.02.2020
+Description : This problem was asked by Ahmet Gungor;
+              n is a parameter with 1 < n < 10 condition;
+              Below figure should be print as output according to n value.
+
+              e.g. n = 4 needs to be as;
+
+              4 4 4 4 4 4 4
+              4 3 3 3 3 3 4
+              4 3 2 2 2 3 4
+              4 3 2 1 2 3 4
+              4 3 2 2 2 3 4
+              4 3 3 3 3 3 4
+              4 4 4 4 4 4 4
+'''
+
+
+class DanceOfNumbers():
+    def __init__(self, n):
+        '''
+        Assigment values
+        '''
+        self.n = n
+
+    def print_shape(self):
+        '''
+        Print figure
+        '''
+        pass
+
+
+if __name__ == "__main__":
+    n = input("Please input n value: ")
+    instance = DanceOfNumbers(n)
+    instance.print_shape()
