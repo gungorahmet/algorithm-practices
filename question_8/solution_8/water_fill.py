a2cac076a835d798f9b9a524de69bcb8aa6a6191
Ahmet Gungor <ahmetgungor92@gmail.com>
2022-02-28 19:21:05 +0300

    folder name is corrected.

--- question_8/solution_8/water_fill.py
+++ question_8/solution_8/water_fill.py
@@ -8,12 +8,14 @@ Date  :       28.02.2022
 
 class Water:
     def __init__(self):
-        self.height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+        # self.height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
         # self.height = [4, 2, 0, 3, 2, 5]  #  To Test
+        # self.height = [4, 3, 2]  #  To Test
         # self.height = [3, 0, 1, 4, 2, 1, 4, 2, 4, 3, 3]  #  To Test
+        # self.height = [4, 0, 0, 1, 2, 5, 3, 1, 0, 4, 3, 2, 5]  #  To Test
 
     def calculate_total_water(self):
-        total_water = 0
+        self.total_water = 0
 
         for row in range(max(self.height), 0, -1):
             tmp_add_water = 0
@@ -22,14 +24,15 @@ class Water:
             for val in self.height:
                 if val >= row:
                     first_wall_found = True
-                    total_water += tmp_add_water
+                    self.total_water += tmp_add_water
                     tmp_add_water = 0
                     continue  # Passing the wall
 
                 if first_wall_found:
                     tmp_add_water += 1
 
-        print(f"Total Water: {total_water}")
+        print(self.height)
+        print(f"Total Water: {self.total_water}")
 
 
 if __name__ == "__main__":
